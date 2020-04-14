4f666cb07c06b73583f44b25fed0b9ddf764f3b7
Jorge de Ugarte <jorgedeugartevalverde@gmail.com>
2020-04-14 10:40:47 -0400

    debugueando

--- app/models/jugador.rb
+++ app/models/jugador.rb
@@ -1,4 +1,4 @@
 class Jugador < ApplicationRecord
-	belongs_to :categorium
-	validates :numero, :nombre, :ranking, :edad, :club_asociacion, :fecha_inscripcion, :status, presence: true
+	belongs_to :categorium, optional: true
+	validates :numero, :nombre, presence: true
 end
